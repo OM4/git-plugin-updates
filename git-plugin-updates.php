825650ee4b9e0d9cf82bcb31dc4d6d398a411194
James Collins <james@om4.com.au>
2013-11-15 14:48:00 +1100

    Use 'release' branch for update checks. master/develop will be used for development.

--- git-plugin-updates.php
+++ git-plugin-updates.php
@@ -9,6 +9,7 @@ Author URI: https://github.com/OM4/om4-plugin-updater/
 License: GPLv2
 License URI: http://www.gnu.org/licenses/gpl-2.0.html
 Git URI: https://github.com/OM4/om4-plugin-updater
+Git Branch: release
 */
 
 /*
